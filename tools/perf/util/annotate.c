460fe827436122ea734e272473e2801e174a23d6
Tom Zanussi <tom.zanussi@intel.com>
2012-10-05 11:35:26 -0500

    perf annotate: replace 'expand' with equivalent sed expression
    We don't have 'expand' in our userspace so we need to accomplish the
    same thing using 'sed', which we do have.
    
    Signed-off-by: Tom Zanussi <tom.zanussi@intel.com>

--- tools/perf/util/annotate.c
+++ tools/perf/util/annotate.c
@@ -1886,7 +1886,7 @@ static int symbol__disassemble(struct symbol *sym, struct annotate_args *args)
 	err = asprintf(&command,
 		 "%s %s%s --start-address=0x%016" PRIx64
 		 " --stop-address=0x%016" PRIx64
-		 " -l -d %s %s -C \"$1\" 2>/dev/null|grep -v \"$1:\"|expand",
+		 " -l -d %s %s -C \"$1\" 2>/dev/null|grep -v \"$1:\"|sed 's/\t/        /g'",
 		 opts->objdump_path ?: "objdump",
 		 opts->disassembler_style ? "-M " : "",
 		 opts->disassembler_style ?: "",
