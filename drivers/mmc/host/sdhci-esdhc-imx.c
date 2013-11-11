f87123ed820073f5663cb2e171dcd642087f2341
Dong Aisheng <b29396@freescale.com>
2013-11-11 14:53:58 +0800

    MLK-11685-2 sdhci-esdhc-imx: use bus freq in runtime pm
    commit 52aaee75992de010361b7663ac7516fc1e2c909c from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    Request BUS_FREQ_HIGH when bus is busy and then release BUS_FREQ_HIGH
    when bus becomes idle.
    
    Signed-off-by: Dong Aisheng <b29396@freescale.com>
    (cherry picked from commit 64994f7115573c9ede53b51536b2c15f7cf0112a)
    Signed-off-by: Haibo Chen <haibo.chen@freescale.com>
    
    Conflicts:
    	drivers/mmc/host/sdhci-esdhc-imx.c
    (Vipul: Fixed merge conflicts)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/mmc/host/sdhci-esdhc-imx.c
+++ drivers/mmc/host/sdhci-esdhc-imx.c
@@ -8,6 +8,7 @@
  *   Author: Wolfram Sang <kernel@pengutronix.de>
  */
 
+#include <linux/busfreq-imx.h>
 #include <linux/io.h>
 #include <linux/delay.h>
 #include <linux/err.h>
@@ -1496,6 +1497,9 @@ static int sdhci_esdhc_imx_probe(struct platform_device *pdev)
 
 	pltfm_host->clk = imx_data->clk_per;
 	pltfm_host->clock = clk_get_rate(pltfm_host->clk);
+
+	request_bus_freq(BUS_FREQ_HIGH);
+
 	err = clk_prepare_enable(imx_data->clk_per);
 	if (err)
 		goto free_sdhci;
@@ -1596,6 +1600,9 @@ disable_ipg_clk:
 	clk_disable_unprepare(imx_data->clk_ipg);
 disable_per_clk:
 	clk_disable_unprepare(imx_data->clk_per);
+
+	release_bus_freq(BUS_FREQ_HIGH);
+
 free_sdhci:
 	if (imx_data->socdata->flags & ESDHC_FLAG_PMQOS)
 		pm_qos_remove_request(&imx_data->pm_qos_req);
@@ -1694,6 +1701,8 @@ static int sdhci_esdhc_runtime_suspend(struct device *dev)
 	}
 	clk_disable_unprepare(imx_data->clk_ahb);
 
+	release_bus_freq(BUS_FREQ_HIGH);
+
 	if (imx_data->socdata->flags & ESDHC_FLAG_PMQOS)
 		pm_qos_remove_request(&imx_data->pm_qos_req);
 
@@ -1714,6 +1723,8 @@ static int sdhci_esdhc_runtime_resume(struct device *dev)
 	err = clk_prepare_enable(imx_data->clk_ahb);
 	if (err)
 		goto remove_pm_qos_request;
+
+	request_bus_freq(BUS_FREQ_HIGH);
 
 	if (!sdhci_sdio_irq_enabled(host)) {
 		err = clk_prepare_enable(imx_data->clk_per);
