764c50de32bcec58e7665cec79cd5c124dc649fa
Shengjiu Wang <shengjiu.wang@freescale.com>
2015-01-27 16:44:34 +0800

    MLK-10161-3: ARM: imx6sx: Add SPDIF_GCLK clock in clock tree
    commit 3b93d5d86888addc3ea16acc838e718e00aae171 from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    As spdif driver will register SPDIF clock to regmap, regmap will do
    clk_prepare in init function, so SPDIF clock is prepared in probe, then its
    root clock (pll clock) is prepared also, which cause the arm can't enter
    low power mode.
    Add SPDIF_GCLK in clock tree which share same gate bits with SPDIF clock.
    Its root clock is ipg clock, and register it to regmap, then the issue
    can be fixed.
    
    Signed-off-by: Shengjiu Wang <shengjiu.wang@freescale.com>
    (cherry picked from commit 3f8999cdb4fabed4f720c6ee23947e19c8fff83f)
    Signed-off-by: Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/clk/imx/clk-imx6sx.c
+++ drivers/clk/imx/clk-imx6sx.c
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (C) 2014 Freescale Semiconductor, Inc.
+ * Copyright (C) 2014-2015 Freescale Semiconductor, Inc.
  */
 
 #include <dt-bindings/clock/imx6sx-clock.h>
