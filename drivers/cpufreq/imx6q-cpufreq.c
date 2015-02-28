8eb2f28439b073434e4e91c7280825ec1355777f
Bai Ping <b51503@freescale.com>
2015-02-28 19:19:56 +0800

    MLK-10271 cpufreq: imx6: improve busfreq operation when wehn setpoint lower than 396MHz
    commit 9aa47e693eafe69e3528b87fda95f652d5192195 from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    for i.MX6SX, according to the latest datasheet, added a 198MHz setpoint in
    cpufreq driver. The 198MHz setpoint is NOT enough to support playing mp3,
    the system will stay at a higher setpoint and high_bus_mode. So when having
    a setpoint lower than 396MHz, make sure when the cpufreq is at 396MHz or
    lower, the busfreq is always in low_bus_mode to save more power.
    
    Signed-off-by: Bai Ping <b51503@freescale.com>
    (cherry picked from commit 3ba9548200ffb2c85111dd84946046ae0c7b09c4)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/cpufreq/imx6q-cpufreq.c
+++ drivers/cpufreq/imx6q-cpufreq.c
@@ -19,6 +19,7 @@
 #define PU_SOC_VOLTAGE_NORMAL	1250000
 #define PU_SOC_VOLTAGE_HIGH	1275000
 #define FREQ_1P2_GHZ		1200000000
+#define FREQ_396_MHZ		396000
 
 struct regulator *arm_reg;
 static struct regulator *pu_reg;
@@ -94,7 +95,7 @@ static int imx6q_set_target(struct cpufreq_policy *policy, unsigned int index)
 	 * CPU freq is increasing, so need to ensure
 	 * that bus frequency is increased too.
 	 */
-	if (old_freq == freq_table[0].frequency)
+	if (old_freq <= FREQ_396_MHZ && new_freq > FREQ_396_MHZ)
 		request_bus_freq(BUS_FREQ_HIGH);
 
 	/* scaling up?  scale voltage before frequency */
@@ -209,7 +210,7 @@ static int imx6q_set_target(struct cpufreq_policy *policy, unsigned int index)
 	 * If CPU is dropped to the lowest level, release the need
 	 * for a high bus frequency.
 	 */
-	if (new_freq == freq_table[0].frequency)
+	if (old_freq > FREQ_396_MHZ && new_freq <= FREQ_396_MHZ)
 		release_bus_freq(BUS_FREQ_HIGH);
 
 	return 0;
@@ -227,10 +228,9 @@ static int imx6q_cpufreq_init(struct cpufreq_policy *policy)
 		dev_err(cpu_dev, "imx6 cpufreq init failed!\n");
 		return ret;
 	}
-	if (policy->cur > freq_table[0].frequency)
+	if (policy->cur > FREQ_396_MHZ)
 		request_bus_freq(BUS_FREQ_HIGH);
 	dev_pm_opp_of_register_em(policy->cpus);
-
 	return 0;
 }
 
