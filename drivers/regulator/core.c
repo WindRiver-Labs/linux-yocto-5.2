76cbf8861a74d4b70166337ffa0f9202891bc7f0
Fancy Fang <chen.fang@freescale.com>
2015-08-21 14:19:27 +0800

    MLK-11395-3 regulator: consumer: add new event macros
    commit ae8f203abc57ed68160fab6725252e66a3e8dda4 from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    Add two new regulator events macro 'REGULATOR_EVENT_PRE_DO_ENABLE'
    and 'REGULATOR_EVENT_PRE_DO_DISABLE', since some gpc operations
    should be required when MIPI PHY is powered on/off.
    
    Signed-off-by: Fancy Fang <chen.fang@freescale.com>
    Signed-off-by: Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/regulator/core.c
+++ drivers/regulator/core.c
@@ -2343,6 +2343,7 @@ static int _regulator_do_enable(struct regulator_dev *rdev)
 {
 	int ret, delay;
 
+	_notifier_call_chain(rdev, REGULATOR_EVENT_PRE_DO_ENABLE, NULL);
 	/* Query before enabling in case configuration dependent.  */
 	ret = _regulator_get_enable_time(rdev);
 	if (ret >= 0) {
@@ -2556,6 +2557,7 @@ static int _regulator_do_disable(struct regulator_dev *rdev)
 {
 	int ret;
 
+	_notifier_call_chain(rdev, REGULATOR_EVENT_PRE_DO_DISABLE, NULL);
 	trace_regulator_disable(rdev_get_name(rdev));
 
 	if (rdev->ena_pin) {
