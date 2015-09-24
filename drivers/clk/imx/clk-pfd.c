79d10110ed2b508593ecb6e7903715c4e697db8c
Bai Ping <b51503@freescale.com>
2015-09-25 00:49:13 +0800

    MLK-11620 ARM: imx: single SOC config/compile support
    commit f0815f579b5d1f76f646129cf001646ffe83319d from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    Currently, all i.MX6 config (CONFIG_SOC_IMX6XXX) are enabled, so
    build and function are OK for every i.MX6 SOC, however, when only
    one SOC config is selected in menu config, for example, users only
    needs i.MX6SL, they might deselect all reset SOC configs, then the
    build will fail, this is unacceptable.
    
    Signed-off-by: Anson Huang <b20788@freescale.com>
    Signed-off-by: Bai Ping <b51503@freescale.com>
    [Arul: Fix merge conflicts]
    Signed-off-by: Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/clk/imx/clk-pfd.c
+++ drivers/clk/imx/clk-pfd.c
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 /*
  * Copyright 2012 Freescale Semiconductor, Inc.
- * Copyright 2012-2014 Freescale Semiconductor, Inc.
+ * Copyright 2012-2015 Freescale Semiconductor, Inc.
  * Copyright 2012 Linaro Ltd.
  */
 
@@ -48,6 +48,7 @@ static void clk_pfd_do_shared_clks(struct clk_hw *hw, bool enable)
 	struct clk_pfd *pfd = to_clk_pfd(hw);
 
 	if (imx_src_is_m4_enabled()) {
+#ifdef CONFIG_SOC_IMX6SX
 		if (!amp_power_mutex || !shared_mem) {
 			if (enable)
 				clk_pfd_do_hardware(pfd, enable);
@@ -69,6 +70,7 @@ static void clk_pfd_do_shared_clks(struct clk_hw *hw, bool enable)
 		clk_pfd_do_hardware(pfd, enable);
 
 		imx_sema4_mutex_unlock(amp_power_mutex);
+#endif
 	} else {
 		clk_pfd_do_hardware(pfd, enable);
 	}
