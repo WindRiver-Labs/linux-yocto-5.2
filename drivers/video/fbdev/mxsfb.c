95242b04804d24280767b48d9a07d3f1f2c61b54
Fancy Fang <chen.fang@nxp.com>
2016-07-12 14:48:40 +0800

    MLK-12980-3 video: mxsfb: free 'pseudo_palette' to avoid memory leak
    commit 6b978ac26a35e8db0ce741a71ca4a7636a06cbec from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    The dynamicly allocated 'pseudo_palette' area should be
    freed when unused to avoid memory leak.
    
    Signed-off-by: Fancy Fang <chen.fang@nxp.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/video/fbdev/mxsfb.c
+++ drivers/video/fbdev/mxsfb.c
@@ -1521,6 +1521,7 @@ fb_destroy:
 	fb_destroy_modelist(&fb_info->modelist);
 fb_pm_runtime_disable:
 	pm_runtime_disable(&host->pdev->dev);
+	devm_kfree(&pdev->dev, fb_info->pseudo_palette);
 fb_release:
 	framebuffer_release(fb_info);
 	devm_kfree(&pdev->dev, host);
@@ -1542,6 +1543,7 @@ static int mxsfb_remove(struct platform_device *pdev)
 
 	platform_set_drvdata(pdev, NULL);
 
+	devm_kfree(&pdev->dev, fb_info->pseudo_palette);
 	framebuffer_release(fb_info);
 	devm_kfree(&pdev->dev, host);
 
