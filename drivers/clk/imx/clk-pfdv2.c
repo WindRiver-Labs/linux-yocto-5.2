fa3694b1d644d40e0e799addd9a41890a54d5ce7
Anson Huang <Anson.Huang@nxp.com>
2016-11-08 00:03:54 +0800

    MLK-13441-5 ARM: imx: add new clk types
    commit cbc709fb0dae07c81cf11575fc1766ad9762e07e from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    Add below new clock types to support new SoC:
    
    composite clk;
    frac-divider;
    pfdv2;
    pllv4.
    
    These clock types are for i.MX7ULP and maybe
    following SoCs.
    
    Signed-off-by: Anson Huang <Anson.Huang@nxp.com>
    Signed-off-by: Bai Ping <ping.bai@nxp.com>
    [Octavian: fix build warning by using u64 in do_div ops]
    Signed-off-by: Octavian Purdila <octavian.purdila@nxp.com>
    Signed-off-by: Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/clk/imx/clk-pfdv2.c
+++ drivers/clk/imx/clk-pfdv2.c
@@ -1,17 +1,19 @@
-// SPDX-License-Identifier: GPL-2.0+
 /*
- * Copyright (C) 2016 Freescale Semiconductor, Inc.
- * Copyright 2017~2018 NXP
+ * Copyright 2016 Freescale Semiconductor, Inc.
  *
- * Author: Dong Aisheng <aisheng.dong@nxp.com>
+ * The code contained herein is licensed under the GNU General Public
+ * License. You may obtain a copy of the GNU General Public License
+ * Version 2 or later at the following locations:
  *
+ * http://www.opensource.org/licenses/gpl-license.html
+ * http://www.gnu.org/copyleft/gpl.html
  */
 
+#include <linux/clk.h>
 #include <linux/clk-provider.h>
-#include <linux/err.h>
 #include <linux/io.h>
-#include <linux/iopoll.h>
 #include <linux/slab.h>
+#include <linux/err.h>
 
 #include "clk.h"
 
@@ -19,78 +21,53 @@
  * struct clk_pfdv2 - IMX PFD clock
  * @clk_hw:	clock source
  * @reg:	PFD register address
- * @gate_bit:	Gate bit offset
- * @vld_bit:	Valid bit offset
- * @frac_off:	PLL Fractional Divider offset
+ * @idx:	the index of PFD encoded in the register
+ *
  */
 
 struct clk_pfdv2 {
 	struct clk_hw	hw;
 	void __iomem	*reg;
-	u8		gate_bit;
-	u8		vld_bit;
-	u8		frac_off;
+	u8		idx;
 };
 
 #define to_clk_pfdv2(_hw) container_of(_hw, struct clk_pfdv2, hw)
 
-#define CLK_PFDV2_FRAC_MASK 0x3f
-
-#define LOCK_TIMEOUT_US		USEC_PER_MSEC
-
-static DEFINE_SPINLOCK(pfd_lock);
-
-static int clk_pfdv2_wait(struct clk_pfdv2 *pfd)
-{
-	u32 val;
-
-	return readl_poll_timeout(pfd->reg, val, val & (1 << pfd->vld_bit),
-				  0, LOCK_TIMEOUT_US);
-}
-
-static int clk_pfdv2_enable(struct clk_hw *hw)
+static int clk_pfd_enable(struct clk_hw *hw)
 {
 	struct clk_pfdv2 *pfd = to_clk_pfdv2(hw);
-	unsigned long flags;
 	u32 val;
 
-	spin_lock_irqsave(&pfd_lock, flags);
 	val = readl_relaxed(pfd->reg);
-	val &= ~(1 << pfd->gate_bit);
+	val &= ~(1 << ((pfd->idx + 1) * 8 - 1));
 	writel_relaxed(val, pfd->reg);
-	spin_unlock_irqrestore(&pfd_lock, flags);
 
-	return clk_pfdv2_wait(pfd);
+	return 0;
 }
 
-static void clk_pfdv2_disable(struct clk_hw *hw)
+static void clk_pfd_disable(struct clk_hw *hw)
 {
 	struct clk_pfdv2 *pfd = to_clk_pfdv2(hw);
-	unsigned long flags;
 	u32 val;
 
-	spin_lock_irqsave(&pfd_lock, flags);
 	val = readl_relaxed(pfd->reg);
-	val |= (1 << pfd->gate_bit);
+	val |= 1 << ((pfd->idx + 1) * 8 - 1);
 	writel_relaxed(val, pfd->reg);
-	spin_unlock_irqrestore(&pfd_lock, flags);
 }
 
-static unsigned long clk_pfdv2_recalc_rate(struct clk_hw *hw,
-					   unsigned long parent_rate)
+static unsigned long clk_pfd_recalc_rate(struct clk_hw *hw,
+					 unsigned long parent_rate)
 {
 	struct clk_pfdv2 *pfd = to_clk_pfdv2(hw);
 	u64 tmp = parent_rate;
-	u8 frac;
+	u8 frac = (readl_relaxed(pfd->reg) >> (pfd->idx * 8)) & 0x3f;
 
-	frac = (readl_relaxed(pfd->reg) >> pfd->frac_off)
-		& CLK_PFDV2_FRAC_MASK;
-
-	if (!frac) {
-		pr_debug("clk_pfdv2: %s invalid pfd frac value 0\n",
-			 clk_hw_get_name(hw));
-		return 0;
-	}
+	/*
+	 * The reset value of pfd field is zero, so add one to avoid div
+	 * by zero, optimize this late.
+	 */
+	if (!frac)
+		frac += 1;
 
 	tmp *= 18;
 	do_div(tmp, frac);
@@ -98,8 +75,8 @@ static unsigned long clk_pfdv2_recalc_rate(struct clk_hw *hw,
 	return tmp;
 }
 
-static long clk_pfdv2_round_rate(struct clk_hw *hw, unsigned long rate,
-				 unsigned long *prate)
+static long clk_pfd_round_rate(struct clk_hw *hw, unsigned long rate,
+			       unsigned long *prate)
 {
 	u64 tmp = *prate;
 	u8 frac;
@@ -107,12 +84,10 @@ static long clk_pfdv2_round_rate(struct clk_hw *hw, unsigned long rate,
 	tmp = tmp * 18 + rate / 2;
 	do_div(tmp, rate);
 	frac = tmp;
-
 	if (frac < 12)
 		frac = 12;
 	else if (frac > 35)
 		frac = 35;
-
 	tmp = *prate;
 	tmp *= 18;
 	do_div(tmp, frac);
@@ -120,21 +95,10 @@ static long clk_pfdv2_round_rate(struct clk_hw *hw, unsigned long rate,
 	return tmp;
 }
 
-static int clk_pfdv2_is_enabled(struct clk_hw *hw)
+static int clk_pfd_set_rate(struct clk_hw *hw, unsigned long rate,
+		unsigned long parent_rate)
 {
 	struct clk_pfdv2 *pfd = to_clk_pfdv2(hw);
-
-	if (readl_relaxed(pfd->reg) & (1 << pfd->gate_bit))
-		return 0;
-
-	return 1;
-}
-
-static int clk_pfdv2_set_rate(struct clk_hw *hw, unsigned long rate,
-			      unsigned long parent_rate)
-{
-	struct clk_pfdv2 *pfd = to_clk_pfdv2(hw);
-	unsigned long flags;
 	u64 tmp = parent_rate;
 	u32 val;
 	u8 frac;
@@ -147,58 +111,58 @@ static int clk_pfdv2_set_rate(struct clk_hw *hw, unsigned long rate,
 	else if (frac > 35)
 		frac = 35;
 
-	spin_lock_irqsave(&pfd_lock, flags);
 	val = readl_relaxed(pfd->reg);
-	val &= ~(CLK_PFDV2_FRAC_MASK << pfd->frac_off);
-	val |= frac << pfd->frac_off;
+	val &= ~(0x3f << (pfd->idx * 8));
+	val |= frac << (pfd->idx * 8);
 	writel_relaxed(val, pfd->reg);
-	spin_unlock_irqrestore(&pfd_lock, flags);
 
 	return 0;
 }
 
+static int clk_pfd_is_enabled(struct clk_hw *hw)
+{
+	struct clk_pfdv2 *pfd = to_clk_pfdv2(hw);
+
+	if (readl_relaxed(pfd->reg) & (1 << ((pfd->idx + 1) * 8 - 1)))
+		return 0;
+
+	return 1;
+}
+
 static const struct clk_ops clk_pfdv2_ops = {
-	.enable		= clk_pfdv2_enable,
-	.disable	= clk_pfdv2_disable,
-	.recalc_rate	= clk_pfdv2_recalc_rate,
-	.round_rate	= clk_pfdv2_round_rate,
-	.set_rate	= clk_pfdv2_set_rate,
-	.is_enabled     = clk_pfdv2_is_enabled,
+	.enable		= clk_pfd_enable,
+	.disable	= clk_pfd_disable,
+	.recalc_rate	= clk_pfd_recalc_rate,
+	.round_rate	= clk_pfd_round_rate,
+	.set_rate	= clk_pfd_set_rate,
+	.is_enabled     = clk_pfd_is_enabled,
 };
 
-struct clk_hw *imx_clk_pfdv2(const char *name, const char *parent_name,
-			     void __iomem *reg, u8 idx)
+struct clk *imx_clk_pfdv2(const char *name, const char *parent_name,
+			void __iomem *reg, u8 idx)
 {
-	struct clk_init_data init;
 	struct clk_pfdv2 *pfd;
-	struct clk_hw *hw;
-	int ret;
-
-	WARN_ON(idx > 3);
+	struct clk *clk;
+	struct clk_init_data init;
 
 	pfd = kzalloc(sizeof(*pfd), GFP_KERNEL);
 	if (!pfd)
 		return ERR_PTR(-ENOMEM);
 
 	pfd->reg = reg;
-	pfd->gate_bit = (idx + 1) * 8 - 1;
-	pfd->vld_bit = pfd->gate_bit - 1;
-	pfd->frac_off = idx * 8;
+	pfd->idx = idx;
 
 	init.name = name;
 	init.ops = &clk_pfdv2_ops;
+	init.flags = 0;
 	init.parent_names = &parent_name;
 	init.num_parents = 1;
-	init.flags = CLK_SET_RATE_GATE;
 
 	pfd->hw.init = &init;
 
-	hw = &pfd->hw;
-	ret = clk_hw_register(NULL, hw);
-	if (ret) {
+	clk = clk_register(NULL, &pfd->hw);
+	if (IS_ERR(clk))
 		kfree(pfd);
-		hw = ERR_PTR(ret);
-	}
 
-	return hw;
+	return clk;
 }
