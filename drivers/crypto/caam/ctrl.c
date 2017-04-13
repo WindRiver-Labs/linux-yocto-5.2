a60e6e0c5be88d2605927d1f3b7729bb0f4209c1
Radu Solea <radu.solea@nxp.com>
2017-04-13 16:14:06 +0300

    MLK-14284 Fix CAAM Errata err005766 handling
    commit a90ba2ea8ed489de12c1e274927997a408a75895 from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    * Update ERA detection code to check 3 sources CCBVID, CAAMVID and
    the device tree.
    * Fix bit handling of CAAMVID data to obtain correct results.
    * Remove default device tree values.
    * Update errata handling to target known affected platforms.
    
    Signed-off-by: Radu Solea <radu.solea@nxp.com>
    (Vipul: Fixed merge conflicts)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/crypto/caam/ctrl.c
+++ drivers/crypto/caam/ctrl.c
@@ -404,56 +404,13 @@ start_rng:
 	clrsetbits_32(&r4tst->rtmctl, RTMCTL_PRGM, RTMCTL_SAMP_MODE_RAW_ES_SC);
 }
 
-static int caam_get_era_from_hw(struct caam_ctrl __iomem *ctrl)
+static void detect_era(struct caam_drv_private *ctrlpriv)
 {
-	static const struct {
-		u16 ip_id;
-		u8 maj_rev;
-		u8 era;
-	} id[] = {
-		{0x0A10, 1, 1},
-		{0x0A10, 2, 2},
-		{0x0A12, 1, 3},
-		{0x0A14, 1, 3},
-		{0x0A14, 2, 4},
-		{0x0A16, 1, 4},
-		{0x0A10, 3, 4},
-		{0x0A11, 1, 4},
-		{0x0A18, 1, 4},
-		{0x0A11, 2, 5},
-		{0x0A12, 2, 5},
-		{0x0A13, 1, 5},
-		{0x0A1C, 1, 5}
-	};
-	u32 ccbvid, id_ms;
-	u8 maj_rev, era;
-	u16 ip_id;
-	int i;
-
-	ccbvid = rd_reg32(&ctrl->perfmon.ccb_id);
-	era = (ccbvid & CCBVID_ERA_MASK) >> CCBVID_ERA_SHIFT;
-	if (era)	/* This is '0' prior to CAAM ERA-6 */
-		return era;
-
-	id_ms = rd_reg32(&ctrl->perfmon.caam_id_ms);
-	ip_id = (id_ms & SECVID_MS_IPID_MASK) >> SECVID_MS_IPID_SHIFT;
-	maj_rev = (id_ms & SECVID_MS_MAJ_REV_MASK) >> SECVID_MS_MAJ_REV_SHIFT;
-
-	for (i = 0; i < ARRAY_SIZE(id); i++)
-		if (id[i].ip_id == ip_id && id[i].maj_rev == maj_rev)
-			return id[i].era;
-
-	return -ENOTSUPP;
-}
-
-/**
- * caam_get_era() - Return the ERA of the SEC on SoC, based
- * on the SEC_VID register.
- * Returns the ERA number (1..4) or -ENOTSUPP if the ERA is unknown.
- * @caam_id - the value of the SEC_VID register
- **/
-int caam_get_era(u64 caam_id)
-{
+	int ret, i;
+	u32 caam_era;
+	u32 caam_id_ms;
+	char *era_source;
+	struct device_node *caam_node;
 	struct sec_vid sec_vid;
 	static const struct {
 		u16 ip_id;
@@ -487,17 +444,45 @@ int caam_get_era(u64 caam_id)
 		{0x0A12, 5, 8},
 		{0x0A16, 3, 8},
 	};
-	int i;
 
-	sec_vid.ip_id = caam_id >> SEC_VID_IPID_SHIFT;
-	sec_vid.maj_rev = (caam_id & SEC_VID_MAJ_MASK) >> SEC_VID_MAJ_SHIFT;
+	/* If the user or bootloader has set the property we'll use that */
+	caam_node = of_find_compatible_node(NULL, NULL, "fsl,sec-v4.0");
+	ret = of_property_read_u32(caam_node, "fsl,sec-era", &caam_era);
+	of_node_put(caam_node);
+
+	if (!ret) {
+		era_source = "device tree";
+		goto era_found;
+	}
+
+	/* If ccbvid has the era, use that (era 6 and onwards) */
+	caam_era = rd_reg32(&ctrlpriv->ctrl->perfmon.ccb_id);
+	caam_era = caam_era >> CCB_VID_ERA_SHIFT & CCB_VID_ERA_MASK;
+
+	if (caam_era) {
+		era_source = "CCBVID";
+		goto era_found;
+	}
+
+	/* If we can match caamvid to known versions, use that */
+	caam_id_ms = rd_reg32(&ctrlpriv->ctrl->perfmon.caam_id_ms);
+	sec_vid.ip_id = caam_id_ms >> SEC_VID_IPID_SHIFT;
+	sec_vid.maj_rev = (caam_id_ms & SEC_VID_MAJ_MASK) >> SEC_VID_MAJ_SHIFT;
 
 	for (i = 0; i < ARRAY_SIZE(caam_eras); i++)
 		if (caam_eras[i].ip_id == sec_vid.ip_id &&
-		    caam_eras[i].maj_rev == sec_vid.maj_rev)
-			return caam_eras[i].era;
+		    caam_eras[i].maj_rev == sec_vid.maj_rev) {
+			caam_era = caam_eras[i].era;
+			era_source = "CAAMVID";
+			goto era_found;
+		}
 
-	return -ENOTSUPP;
+	ctrlpriv->era = -ENOTSUPP;
+	return;
+
+era_found:
+	ctrlpriv->era = caam_era;
+	pr_info("ERA source: %s.\n", era_source);
 }
 
 /*
@@ -667,6 +652,8 @@ static int caam_probe(struct platform_device *pdev)
 			 BLOCK_OFFSET * DECO_BLOCK_NUMBER
 			 );
 
+	detect_era(ctrlpriv);
+
 	/* Get CAAM-SM node and of_iomap() and save */
 	np = of_find_compatible_node(NULL, NULL, "fsl,imx6q-caam-sm");
 
@@ -736,8 +723,6 @@ static int caam_probe(struct platform_device *pdev)
 		dev_err(dev, "dma_set_mask_and_coherent failed (%d)\n", ret);
 		goto iounmap_ctrl;
 	}
-
-	ctrlpriv->era = caam_get_era(ctrl);
 
 	ret = of_platform_populate(nprop, caam_match, NULL, dev);
 	if (ret) {
@@ -872,9 +857,8 @@ static int caam_probe(struct platform_device *pdev)
 	caam_id = (u64)rd_reg32(&ctrl->perfmon.caam_id_ms) << 32 |
 		  (u64)rd_reg32(&ctrl->perfmon.caam_id_ls);
 
-	/* Report "alive" for developer to see */
 	dev_info(dev, "device ID = 0x%016llx (Era %d)\n", caam_id,
-		 caam_get_era(caam_id));
+		 ctrlpriv->era);
 	dev_info(dev, "job rings = %d, qi = %d, dpaa2 = %s\n",
 		 ctrlpriv->total_jobrs, ctrlpriv->qi_present,
 		 caam_dpaa2 ? "yes" : "no");
