3eb22c7a4f61b278015dc5c3082c39ec0dc56f86
Uma Shankar <uma.shankar@intel.com>
2017-05-31 15:40:49 +0530

    drm: Add HDR source metadata property
    commit 39486f39894f30d482fdd6356188e15393282e0c from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    This patch adds a blob property to get HDR metadata
    information from userspace. This will be send as part
    of AVI Infoframe to panel.
    
    Signed-off-by: Uma Shankar <uma.shankar@intel.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- include/drm/drm_connector.h
+++ include/drm/drm_connector.h
@@ -599,6 +599,13 @@ struct drm_connector_state {
 	 * and the connector bpc limitations obtained from edid.
 	 */
 	u8 max_bpc;
+
+	/**
+	 * @metadata_blob_ptr:
+	 * DRM blob property for HDR metadata
+	 */
+	struct drm_property_blob *hdr_source_metadata_blob_ptr;
+	bool hdr_metadata_changed : 1;
 };
 
 /**
@@ -1239,6 +1246,10 @@ struct drm_connector {
 	 * &drm_mode_config.connector_free_work.
 	 */
 	struct llist_node free_node;
+
+	/* HDR metdata */
+	struct hdr_static_metadata *hdr_panel_metadata;
+	struct hdr_static_metadata *hdr_source_metadata;
 };
 
 #define obj_to_connector(x) container_of(x, struct drm_connector, base)
