2d8695fae4a902f8bf5e569357f844d47c3c9861
Fugang Duan <fugang.duan@nxp.com>
2017-07-18 17:45:00 +0800

    MLK-16023-01 net: phy: at803x: cleared the txc/rxc clk delay enable bits
    commit a637a7914bfa1f75917b0b835fe4fb65b7a18d2c from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    RXC clock delayed bit is enabled in HW reset in default, and
    to avoid uboot set RXC/TXC clk delayed bits, it should clear
    these bits firstly.
    
    Signed-off-by: Fugang Duan <fugang.duan@nxp.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/net/phy/at803x.c
+++ drivers/net/phy/at803x.c
@@ -119,16 +119,26 @@ static int at803x_debug_reg_mask(struct phy_device *phydev, u16 reg,
 	return phy_write(phydev, AT803X_DEBUG_DATA, val);
 }
 
-static int at803x_enable_rx_delay(struct phy_device *phydev)
+static int at803x_set_rx_delay(struct phy_device *phydev,
+				      bool is_enabled)
 {
-	return at803x_debug_reg_mask(phydev, AT803X_DEBUG_REG_0, 0,
-				     AT803X_DEBUG_RX_CLK_DLY_EN);
+	if (is_enabled)
+		return at803x_debug_reg_mask(phydev, AT803X_DEBUG_REG_0, 0,
+						AT803X_DEBUG_RX_CLK_DLY_EN);
+	else
+		return at803x_debug_reg_mask(phydev, AT803X_DEBUG_REG_0,
+						AT803X_DEBUG_RX_CLK_DLY_EN, 0);
 }
 
-static int at803x_enable_tx_delay(struct phy_device *phydev)
+static int at803x_set_tx_delay(struct phy_device *phydev,
+				      bool is_enabled)
 {
-	return at803x_debug_reg_mask(phydev, AT803X_DEBUG_REG_5, 0,
-				     AT803X_DEBUG_TX_CLK_DLY_EN);
+	if (is_enabled)
+		return at803x_debug_reg_mask(phydev, AT803X_DEBUG_REG_5, 0,
+						AT803X_DEBUG_TX_CLK_DLY_EN);
+	else
+		return at803x_debug_reg_mask(phydev, AT803X_DEBUG_REG_5,
+						AT803X_DEBUG_TX_CLK_DLY_EN, 0);
 }
 
 static int at803x_disable_rx_delay(struct phy_device *phydev)
@@ -339,12 +349,22 @@ static int at803x_config_init(struct phy_device *phydev)
 	if (ret < 0)
 		return ret;
 
+	/* Firstly clear the default status in HW reset or
+	 * the bits set by bootloader.
+	 */
+	ret = at803x_set_rx_delay(phydev, false);
+	if (ret < 0)
+		return ret;
+	ret = at803x_set_tx_delay(phydev, false);
+	if (ret < 0)
+		return ret;
+
 	if (phydev->interface == PHY_INTERFACE_MODE_RGMII_ID ||
 	    phydev->interface == PHY_INTERFACE_MODE_RGMII_RXID) {
 		/* If RGMII_ID or RGMII_RXID are specified enable RX delay,
 		 * otherwise keep it disabled
 		 */
-		ret = at803x_enable_rx_delay(phydev);
+		ret = at803x_set_rx_delay(phydev, true);
 		if (ret < 0)
 			return ret;
 	}
@@ -354,7 +374,7 @@ static int at803x_config_init(struct phy_device *phydev)
 		/* If RGMII_ID or RGMII_TXID are specified enable TX delay,
 		 * otherwise keep it disabled
 		 */
-		ret = at803x_enable_tx_delay(phydev);
+		ret = at803x_set_tx_delay(phydev, true);
 	}
 
 	if (priv->quirks & AT803X_VDDIO_1P8V) {
