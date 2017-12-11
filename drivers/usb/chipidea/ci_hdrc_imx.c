0ed2ab64db2970f425398336639aa5a53597251e
Peter Chen <peter.chen@nxp.com>
2017-12-11 11:29:07 +0800

    MLK-16715-7 usb: chipidea: imx: add "ci-disable-lpm" quirk
    commit da9b89cd2ec12f6cfe0d3c1f6dee405175f0bddc from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    Some chipidea hardware needs to disable low power mode for controller
    due to IC issue or hardware issue, add one quirk for it.
    
    TODO: checkpatch warning
    
    Acked-by: Jun Li <jun.li@nxp.com>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    (Vipul: Fixed merge conflicts)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Srikanth Krishnakar <Srikanth_Krishnakar@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/usb/chipidea/ci_hdrc_imx.c
+++ drivers/usb/chipidea/ci_hdrc_imx.c
@@ -552,6 +552,11 @@ static int ci_hdrc_imx_probe(struct platform_device *pdev)
 	if (pdata.flags & CI_HDRC_SUPPORTS_RUNTIME_PM)
 		data->supports_runtime_pm = true;
 
+	if (of_find_property(np, "ci-disable-lpm", NULL)) {
+		data->supports_runtime_pm = false;
+		pdata.flags &= ~CI_HDRC_SUPPORTS_RUNTIME_PM;
+	}
+
 	if (of_find_property(np, "fsl,anatop", NULL) && data->usbmisc_data) {
 		data->anatop = syscon_regmap_lookup_by_phandle(np,
 							"fsl,anatop");
