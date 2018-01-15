cc2e4facaa0473ade6277f1cf29af4b81ef7b896
Suman Anna <s-anna@ti.com>
2018-01-15 12:01:54 -0600

    remoteproc: add api for retrieving a rproc unique id
    commit 6cb5846535c6d1af69840582d2ba4e3de8da22d4 from
    https://git.ti.com/cgit/processor-sdk/processor-sdk-linux/?h=processor-sdk-linux-01.00.00
    
    A new api, rproc_get_id(), is added to allow remoteproc/rpmsg clients
    to be able to retrieve a fixed unique id associated with a rproc
    device. Each of the rprocs is given an ida index as well, but this
    is dynamic depending on the order rproc_alloc is called (probe order),
    and also depends on what remote processor devices are instantiated.
    This unique id needs to be supported for both DT and non-DT devices.
    The platform device id or the remoteproc alias id are therefore
    chosen for non-DT and DT devices respectively, with the assumption
    that the remoteproc devices are created with known platform device
    ids or alias ids in DT.
    
    Signed-off-by: Suman Anna <s-anna@ti.com>
    Signed-off-by: Xiaolei Wang <xiaolei.wang@windriver.com>

--- include/linux/remoteproc.h
+++ include/linux/remoteproc.h
@@ -592,6 +592,7 @@ int rproc_boot(struct rproc *rproc);
 void rproc_shutdown(struct rproc *rproc);
 void rproc_report_crash(struct rproc *rproc, enum rproc_crash_type type);
 int rproc_coredump_add_segment(struct rproc *rproc, dma_addr_t da, size_t size);
+int rproc_get_id(struct rproc *rproc);
 int rproc_coredump_add_custom_segment(struct rproc *rproc,
 				      dma_addr_t da, size_t size,
 				      void (*dumpfn)(struct rproc *rproc,
