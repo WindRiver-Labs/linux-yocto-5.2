c4c60ed32661d357dc786f80faea9ee9c25f0278
Costin Carabas <costin.carabas@nxp.com>
2018-05-22 18:09:12 +0300

    can: Add compatible for s32gen1
    commit abe3af4674402939df56ff2325d7849672d38edd from
    https://source.codeaurora.org/external/autobsps32/linux
    
    Issue: ALB-2410
    Signed-off-by: Costin Carabas <costin.carabas@nxp.com>
    Signed-off-by: Xulin Sun <xulin.sun@windriver.com>

--- drivers/net/can/flexcan.c
+++ drivers/net/can/flexcan.c
@@ -325,6 +325,10 @@ static struct flexcan_devtype_data fsl_s32v234_devtype_data = {
 	.quirks = FLEXCAN_QUIRK_DISABLE_RXFG | FLEXCAN_QUIRK_DISABLE_MECR,
 };
 
+static struct flexcan_devtype_data fsl_s32gen1_devtype_data = {
+	.quirks = FLEXCAN_QUIRK_DISABLE_RXFG | FLEXCAN_QUIRK_DISABLE_MECR,
+};
+
 static const struct can_bittiming_const flexcan_bittiming_const = {
 	.name = DRV_NAME,
 	.tseg1_min = 4,
@@ -1499,6 +1503,7 @@ static const struct of_device_id flexcan_of_match[] = {
 	{ .compatible = "fsl,p1010-flexcan", .data = &fsl_p1010_devtype_data, },
 	{ .compatible = "fsl,vf610-flexcan", .data = &fsl_vf610_devtype_data, },
 	{ .compatible = "fsl,s32v234-flexcan", .data = &fsl_s32v234_devtype_data, },
+	{ .compatible = "fsl,s32gen1-flexcan", .data = &fsl_s32gen1_devtype_data, },
 	{ .compatible = "fsl,ls1021ar2-flexcan", .data = &fsl_ls1021a_r2_devtype_data, },
 	{ /* sentinel */ },
 };
