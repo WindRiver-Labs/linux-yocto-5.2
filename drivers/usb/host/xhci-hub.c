74fc329747698b1d91bd85c9df9fe8c46b4eae1a
Peter Chen <peter.chen@nxp.com>
2018-09-04 16:04:14 +0800

    MLK-19442-1 usb: host: xhci: export xhci_bus_suspend
    commit d9c16ef06ed7890f3ec2cad206de9a50ae6223bf from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    It may be used by xhci platform driver, fixed below error when
    building xhci as module.
    
    ERROR: "xhci_bus_suspend" [drivers/usb/cdns3/cdns3.ko] undefined!
    linux-imx/scripts/Makefile.modpost:92: recipe for target '__modpost' failed
    make[2]: *** [__modpost] Error 1
    linux-imx/Makefile:1231: recipe for target 'modules' failed
    make[1]: *** [modules] Error 2
    
    Reported-by: Bruce Zhang <bo.zhang@nxp.com>
    Signed-off-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Srikanth Krishnakar <Srikanth_Krishnakar@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/usb/host/xhci-hub.c
+++ drivers/usb/host/xhci-hub.c
@@ -1656,6 +1656,7 @@ retry:
 	spin_unlock_irqrestore(&xhci->lock, flags);
 	return 0;
 }
+EXPORT_SYMBOL(xhci_bus_suspend);
 
 /*
  * Workaround for missing Cold Attach Status (CAS) if device re-plugged in S3.
