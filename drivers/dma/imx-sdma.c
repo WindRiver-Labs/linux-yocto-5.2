2cd7014db7c54bb6f0e4662a1f66c1bbb41e53e0
Robin Gong <yibin.gong@nxp.com>
2018-09-06 18:26:04 +0800

    MLK-19418-3: dmaengine: imx-sdma: remove context load in sdma_channel_resume
    commit f4d7fcb487aefabc1c748a56dc4aa924133d9825 from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    Since sdma_resume will restore all context in case sdma power off after
    suspend, sdma_channel_resume does not need to restore again. Besides,
    'suspend_off' can be removed too.
    
    Signed-off-by: Robin Gong <yibin.gong@nxp.com>
    Reviewed-by: Shengjiu Wang <shengjiu.wang@nxp.com>
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Srikanth Krishnakar <Srikanth_Krishnakar@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/dma/imx-sdma.c
+++ drivers/dma/imx-sdma.c
@@ -476,7 +476,6 @@ struct sdma_engine {
 	struct sdma_buffer_descriptor	*bd0;
 	/* clock ratio for AHB:SDMA core. 1:1 is 1, 2:1 is 0*/
 	bool				clk_ratio;
-	bool				suspend_off;
 	bool				fw_loaded;
 	int				idx;
 };
@@ -1447,21 +1446,10 @@ static int sdma_channel_pause(struct dma_chan *chan)
 static int sdma_channel_resume(struct dma_chan *chan)
 {
 	struct sdma_channel *sdmac = to_sdma_chan(chan);
-	struct sdma_engine *sdma = sdmac->sdma;
 	unsigned long flags;
 
 	if (!(sdmac->flags & IMX_DMA_SG_LOOP))
 		return -EINVAL;
-
-	/*
-	 * restore back context since context may loss if mega/fast OFF
-	 */
-	if (sdma->suspend_off) {
-		if (sdma_load_context(sdmac)) {
-			dev_err(sdmac->sdma->dev, "context load failed.\n");
-			return -EINVAL;
-		}
-	}
 
 	sdma_enable_channel(sdmac->sdma, sdmac->channel);
 	spin_lock_irqsave(&sdmac->vc.lock, flags);
@@ -2603,8 +2591,6 @@ static int sdma_suspend(struct device *dev)
 	struct sdma_engine *sdma = platform_get_drvdata(pdev);
 	int i, ret = 0;
 
-	sdma->suspend_off = false;
-
 	/* Do nothing if not i.MX6SX or i.MX7D*/
 	if (sdma->drvdata != &sdma_imx6sx && sdma->drvdata != &sdma_imx7d
 	    && sdma->drvdata != &sdma_imx6ul)
@@ -2661,7 +2647,6 @@ static int sdma_resume(struct device *dev)
 
 	/* Firmware was lost, mark as "not ready" */
 	sdma->fw_loaded = false;
-	sdma->suspend_off = true;
 
 	/* restore regs and load firmware */
 	for (i = 0; i < MXC_SDMA_SAVED_REG_NUM; i++) {
