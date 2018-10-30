c0fee8923e4fca618cdea7ea8d86c1c7fdb62aa5
Huazhong Tan <tanhuazhong@huawei.com>
2018-10-30 21:50:46 +0800

    net: hns3: bugfix for the initialization of command queue's spin lock
    commit b2f74dbaf12bf59ff35d451005b3cdee78232ff0 upstream.
    
    The spin lock of the command queue only need to be initialized once
    when the driver initializes the command queue. It is not necessary to
    initialize the spin lock when resetting. At the same time, the
    modification of the queue member should be performed after acquiring
    the lock.
    
    Fixes: 3efb960f056d ("net: hns3: Refactor the initialization of command queue")
    Signed-off-by: Huazhong Tan <tanhuazhong@huawei.com>
    Signed-off-by: David S. Miller <davem@davemloft.net>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_cmd.c
+++ drivers/net/ethernet/hisilicon/hns3/hns3pf/hclge_cmd.c
@@ -327,6 +327,10 @@ int hclge_cmd_queue_init(struct hclge_dev *hdev)
 {
 	int ret;
 
+	/* Setup the lock for command queue */
+	spin_lock_init(&hdev->hw.cmq.csq.lock);
+	spin_lock_init(&hdev->hw.cmq.crq.lock);
+
 	/* Setup the queue entries for use cmd queue */
 	hdev->hw.cmq.csq.desc_num = HCLGE_NIC_CMQ_DESC_NUM;
 	hdev->hw.cmq.crq.desc_num = HCLGE_NIC_CMQ_DESC_NUM;
@@ -360,16 +364,18 @@ int hclge_cmd_init(struct hclge_dev *hdev)
 	u32 version;
 	int ret;
 
+	spin_lock_bh(&hdev->hw.cmq.csq.lock);
+	spin_lock_bh(&hdev->hw.cmq.crq.lock);
+
 	hdev->hw.cmq.csq.next_to_clean = 0;
 	hdev->hw.cmq.csq.next_to_use = 0;
 	hdev->hw.cmq.crq.next_to_clean = 0;
 	hdev->hw.cmq.crq.next_to_use = 0;
 
-	/* Setup the lock for command queue */
-	spin_lock_init(&hdev->hw.cmq.csq.lock);
-	spin_lock_init(&hdev->hw.cmq.crq.lock);
-
 	hclge_cmd_init_regs(&hdev->hw);
+
+	spin_unlock_bh(&hdev->hw.cmq.crq.lock);
+	spin_unlock_bh(&hdev->hw.cmq.csq.lock);
 	clear_bit(HCLGE_STATE_CMD_DISABLE, &hdev->state);
 
 	ret = hclge_cmd_query_firmware_version(&hdev->hw, &version);
