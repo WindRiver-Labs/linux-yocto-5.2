75d2270962ead696c563b9f8df28fac4a21c28d9
Andy Duan <fugang.duan@nxp.com>
2018-12-27 18:32:07 +0530

    MLK-10274 tty: serial: clear AWAKE bit after resume back
    commit 1ca0dcb92c9e484865195ebe6a392a4343cd21ac from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    When uart port (non-console port) in dma mode and use uart as remote wakeup
    source, after resume back, the AWAKE bit is not cleared most of interrupts
    are disabled. And then let system suspend state, system resume back
    immediately without any wake up action. So we need to clear the AWAKE bit
    after resum back in DMA mode.
    
    Merge comments: it is necessary to clear AWAKE or RTSD bit.
    
    Signed-off-by: Fugang Duan <B38611@freescale.com>
    (merge patch from commit: d4b6b6b20eb2c5cca47292f89ab588b15d3be0a9)
    Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/tty/serial/imx.c
+++ drivers/tty/serial/imx.c
@@ -2534,6 +2534,7 @@ static int imx_uart_suspend_noirq(struct device *dev)
 static int imx_uart_resume_noirq(struct device *dev)
 {
 	struct imx_port *sport = dev_get_drvdata(dev);
+	unsigned int usr1;
 	int ret;
 
 	pinctrl_pm_select_default_state(dev);
@@ -2546,6 +2547,9 @@ static int imx_uart_resume_noirq(struct device *dev)
 
 	/* disable wakeup from i.MX UART */
 	imx_uart_enable_wakeup(sport, false);
+	usr1 = readl(sport->port.membase + USR1);
+	if (usr1 & (USR1_AWAKE | USR1_RTSD))
+		writel(USR1_AWAKE | USR1_RTSD, sport->port.membase + USR1);
 
 	clk_disable(sport->clk_ipg);
 
