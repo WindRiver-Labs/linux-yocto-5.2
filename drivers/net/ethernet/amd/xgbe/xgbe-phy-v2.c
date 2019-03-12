ccc9e6ae38a14193f9f90965e425fe1e0136b7f6
Sudheesh Mavila <sudheesh.mavila@amd.com>
2019-03-12 22:33:11 +0530

    amd-xgbe Sometimes driver report incorrect link status and results in timeout error in driver EMBSWDEV-6825
    Signed-off-by: Sudheesh Mavila <sudheesh.mavila@amd.com>
    Signed-off-by: Zhang Xiao <xiao.zhang@windriver.com>
    Signed-off-by: Liwei Song <liwei.song@windriver.com>

--- drivers/net/ethernet/amd/xgbe/xgbe-phy-v2.c
+++ drivers/net/ethernet/amd/xgbe/xgbe-phy-v2.c
@@ -2572,8 +2572,18 @@ static int xgbe_phy_link_status(struct xgbe_prv_data *pdata, int *an_restart)
 	 */
 	reg = XMDIO_READ(pdata, MDIO_MMD_PCS, MDIO_STAT1);
 	reg = XMDIO_READ(pdata, MDIO_MMD_PCS, MDIO_STAT1);
-	if (reg & MDIO_STAT1_LSTATUS)
-		return 1;
+	if(phy_data->sfp_speed == XGBE_SFP_SPEED_10000) {
+		if ((reg & MDIO_STAT1_LSTATUS) && !(reg & MDIO_STAT1_FAULT)) {
+			return 1;
+		} else {
+			*an_restart = 1;
+			pdata->phy_if.phy_reset(pdata);
+			return 0;
+		}
+	} else {
+		if (reg & MDIO_STAT1_LSTATUS)
+			return 1;
+	}
 
 	/* No link, attempt a receiver reset cycle */
 	if (phy_data->rrc_count++ > XGBE_RRC_FREQUENCY) {
