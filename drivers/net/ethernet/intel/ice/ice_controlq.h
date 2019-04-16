0a76b357ef2cab8d975a17865bfa86b3b21dd747
Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
2019-04-16 10:35:03 -0700

    ice: Trivial cosmetic changes
    commit 2f2da36ebf42ef73a4591870b805f5ad0fbc10d5 upstream.
    
    This patch mostly capitalizes abbreviations in code comments. Fixed some
    typos and removed some unnecessary newlines as well.
    
    Signed-off-by: Anirudh Venkataramanan <anirudh.venkataramanan@intel.com>
    Tested-by: Andrew Bowers <andrewx.bowers@intel.com>
    Signed-off-by: Jeff Kirsher <jeffrey.t.kirsher@intel.com>
    Signed-off-by: Liwei Song <liwei.song@windriver.com>

--- drivers/net/ethernet/intel/ice/ice_controlq.h
+++ drivers/net/ethernet/intel/ice/ice_controlq.h
@@ -36,7 +36,7 @@ enum ice_ctl_q {
 #define ICE_CTL_Q_SQ_CMD_USEC		100   /* Check every 100usec */
 
 struct ice_ctl_q_ring {
-	void *dma_head;			/* Virtual address to dma head */
+	void *dma_head;			/* Virtual address to DMA head */
 	struct ice_dma_mem desc_buf;	/* descriptor ring memory */
 	void *cmd_buf;			/* command buffer memory */
 
