cb38c9a00313c43dd8455583de50c88a4139efbe
Sebastian Andrzej Siewior <bigeasy@linutronix.de>
2019-05-29 21:09:09 +0200

    Revert "drm/i915/fence: Do not use TIMER_IRQSAFE"
    The TIMER_IRQSAFE flag is required because the i915 cancels timer from
    interrupt.
    
    Signed-off-by: Sebastian Andrzej Siewior <bigeasy@linutronix.de>

--- drivers/gpu/drm/i915/i915_sw_fence.c
+++ drivers/gpu/drm/i915/i915_sw_fence.c
@@ -461,7 +461,8 @@ int i915_sw_fence_await_dma_fence(struct i915_sw_fence *fence,
 		timer->dma = dma_fence_get(dma);
 		init_irq_work(&timer->work, irq_i915_sw_fence_work);
 
-		timer_setup(&timer->timer, timer_i915_sw_fence_wake, 0);
+		timer_setup(&timer->timer,
+			    timer_i915_sw_fence_wake, TIMER_IRQSAFE);
 		mod_timer(&timer->timer, round_jiffies_up(jiffies + timeout));
 
 		func = dma_i915_sw_fence_wake_timer;
