b4b100ee5fa79f77b430d10900c5d5ea2bfa9d03
Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
2019-06-03 15:57:49 +0100

    iommu: Add recoverable fault reporting
    Some IOMMU hardware features, for example PCI PRI and Arm SMMU Stall,
    enable recoverable I/O page faults. Allow IOMMU drivers to report PRI Page
    Requests and Stall events through the new fault reporting API. The
    consumer of the fault can be either an I/O page fault handler in the host,
    or a guest OS.
    
    Once handled, the fault must be completed by sending a page response back
    to the IOMMU. Add an iommu_page_response() function to complete a page
    fault.
    
    There are two ways to extend the userspace API:
    * Add a field to iommu_page_response and a flag to
      iommu_page_response::flags describing the validity of this field.
    * Introduce a new iommu_page_response_X structure with a different version
      number. The kernel must then support both versions.
    
    Signed-off-by: Jacob Pan <jacob.jun.pan@linux.intel.com>
    Signed-off-by: Jean-Philippe Brucker <jean-philippe.brucker@arm.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    [WK: The original patch got from Marvell sdk11.21.09]
    Signed-off-by: Wenlin Kang <wenlin.kang@windriver.com>

--- include/linux/iommu.h
+++ include/linux/iommu.h
@@ -215,6 +215,7 @@ struct iommu_sva_ops {
  * @sva_bind: Bind process address space to device
  * @sva_unbind: Unbind process address space from device
  * @sva_get_pasid: Get PASID associated to a SVA handle
+ * @page_response: handle page request response
  * @pgsize_bitmap: bitmap of all possible supported page sizes
  */
 struct iommu_ops {
@@ -275,6 +276,10 @@ struct iommu_ops {
 	void (*sva_unbind)(struct iommu_sva *handle);
 	int (*sva_get_pasid)(struct iommu_sva *handle);
 
+	int (*page_response)(struct device *dev,
+			     struct iommu_fault_event *evt,
+			     struct iommu_page_response *msg);
+
 	unsigned long pgsize_bitmap;
 };
 
@@ -299,19 +304,25 @@ struct iommu_device {
  * unrecoverable faults such as DMA or IRQ remapping faults.
  *
  * @fault: fault descriptor
+ * @list: pending fault event list, used for tracking responses
  */
 struct iommu_fault_event {
 	struct iommu_fault fault;
+	struct list_head list;
 };
 
 /**
  * struct iommu_fault_param - per-device IOMMU fault data
  * @handler: Callback function to handle IOMMU faults at device level
  * @data: handler private data
+ * @faults: holds the pending faults which needs response
+ * @lock: protect pending faults list
  */
 struct iommu_fault_param {
 	iommu_dev_fault_handler_t handler;
 	void *data;
+	struct list_head faults;
+	struct mutex lock;
 };
 
 /**
@@ -426,6 +437,8 @@ extern int iommu_unregister_device_fault_handler(struct device *dev);
 
 extern int iommu_report_device_fault(struct device *dev,
 				     struct iommu_fault_event *evt);
+extern int iommu_page_response(struct device *dev,
+			       struct iommu_page_response *msg);
 
 extern int iommu_group_id(struct iommu_group *group);
 extern struct iommu_group *iommu_group_get_for_dev(struct device *dev);
@@ -755,6 +768,12 @@ static inline int iommu_unregister_device_fault_handler(struct device *dev)
 
 static inline
 int iommu_report_device_fault(struct device *dev, struct iommu_fault_event *evt)
+{
+	return -ENODEV;
+}
+
+static inline int iommu_page_response(struct device *dev,
+				      struct iommu_page_response *msg)
 {
 	return -ENODEV;
 }
