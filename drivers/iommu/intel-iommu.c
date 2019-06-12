abc7f1f03889531443642ca4585a31e0b23b3682
Lu Baolu <baolu.lu@linux.intel.com>
2019-06-12 08:28:47 +0800

    iommu/vt-d: Don't enable iommu's which have been ignored
    commit 6a8c6748b94b0968a2f72d4bd2231eae703c0b13 upstream.
    
    The iommu driver will ignore some iommu units if there's no
    device under its scope or those devices have been explicitly
    set to bypass the DMA translation. Don't enable those iommu
    units, otherwise the devices under its scope won't work.
    
    Fixes: d8190dc638866 ("iommu/vt-d: Enable DMA remapping after rmrr mapped")
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Ovidiu Panait <ovidiu.panait@windriver.com>

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -3277,7 +3277,12 @@ static int __init init_dmars(void)
 		goto error;
 	}
 
-	for_each_active_iommu(iommu, drhd) {
+	for_each_iommu(iommu, drhd) {
+		if (drhd->ignored) {
+			iommu_disable_translation(iommu);
+			continue;
+		}
+
 		/*
 		 * Find the max pasid size of all IOMMU's in the system.
 		 * We need to ensure the system pasid table is no bigger
@@ -4838,7 +4843,7 @@ int __init intel_iommu_init(void)
 
 	/* Finally, we enable the DMA remapping hardware. */
 	for_each_iommu(iommu, drhd) {
-		if (!translation_pre_enabled(iommu))
+		if (!drhd->ignored && !translation_pre_enabled(iommu))
 			iommu_enable_translation(iommu);
 
 		iommu_disable_protect_mem_regions(iommu);
