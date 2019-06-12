90db42e576124ea63136210faa67233962b30c43
Lu Baolu <baolu.lu@linux.intel.com>
2019-06-12 08:28:45 +0800

    iommu/vt-d: Don't return error when device gets right domain
    commit 036a857bb57dfc5b6866e924799aef820e842b53 upstream.
    
    If a device gets a right domain in add_device ops, it shouldn't
    return error.
    
    Fixes: 942067f1b6b97 ("iommu/vt-d: Identify default domains replaced with private")
    Signed-off-by: Lu Baolu <baolu.lu@linux.intel.com>
    Signed-off-by: Joerg Roedel <jroedel@suse.de>
    Signed-off-by: Ovidiu Panait <ovidiu.panait@windriver.com>

--- drivers/iommu/intel-iommu.c
+++ drivers/iommu/intel-iommu.c
@@ -5362,10 +5362,7 @@ static int intel_iommu_add_device(struct device *dev)
 				domain_add_dev_info(si_domain, dev);
 				dev_info(dev,
 					 "Device uses a private identity domain.\n");
-				return 0;
 			}
-
-			return -ENODEV;
 		}
 	} else {
 		if (device_def_domain_type(dev) == IOMMU_DOMAIN_DMA) {
@@ -5380,10 +5377,7 @@ static int intel_iommu_add_device(struct device *dev)
 
 				dev_info(dev,
 					 "Device uses a private dma domain.\n");
-				return 0;
 			}
-
-			return -ENODEV;
 		}
 	}
 
