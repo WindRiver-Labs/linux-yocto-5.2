131f774c48f3a05dd6bf72a2bfecf56252fa8287
Zhang Rui <rui.zhang@intel.com>
2019-07-10 21:44:35 +0800

    powercap/intel_rapl: add support for IceLake desktop
    commit 0ab74bcd1b50821391b264150d26b7f03ba6740b upstream.
    
    Add IceLake desktop support in intel_rapl driver
    
    Signed-off-by: Gayatri Kammela <gayatri.kammela@intel.com>
    Signed-off-by: Joe Konno <joe.konno@intel.com>
    Signed-off-by: Zhang Rui <rui.zhang@intel.com>
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    [Jun Miao: context change for backporting]
    Signed-off-by: Jun Miao <jun.miao@windriver.com>

--- drivers/powercap/intel_rapl.c
+++ drivers/powercap/intel_rapl.c
@@ -1150,6 +1150,7 @@ static const struct x86_cpu_id rapl_ids[] __initconst = {
 	INTEL_CPU_FAM6(KABYLAKE_DESKTOP,	rapl_defaults_core),
 	INTEL_CPU_FAM6(CANNONLAKE_MOBILE,	rapl_defaults_core),
 	INTEL_CPU_FAM6(ICELAKE_MOBILE,		rapl_defaults_core),
+	INTEL_CPU_FAM6(ICELAKE_DESKTOP, 	rapl_defaults_core),
 	INTEL_CPU_FAM6(COMETLAKE_L, rapl_defaults_core),
 	INTEL_CPU_FAM6(COMETLAKE, rapl_defaults_core),
 
