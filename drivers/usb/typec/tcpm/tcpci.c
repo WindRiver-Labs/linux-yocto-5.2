1c87032cebf76d1ee63a4dee1a3896de34149d85
Li Jun <jun.li@nxp.com>
2019-07-24 13:24:25 +0800

    MLK-22308 usb: typec: tcpci: mark PM functions as __maybe_unused
    commit a56431a455e517ea8db231d0a003ef8d4d6079be from
    https://source.codeaurora.org/external/imx/linux-imx.git
    
    Fix build warning if CONFIG_PM_SLEEP is disabled:
    
      CC      drivers/usb/typec/tcpm/tcpci.o
    drivers/usb/typec/tcpm/tcpci.c:849:12: warning: ‘tcpci_resume’ defined but
    not used [-Wunused-function]
     static int tcpci_resume(struct device *dev)
                ^~~~~~~~~~~~
    drivers/usb/typec/tcpm/tcpci.c:837:12: warning: ‘tcpci_suspend’ defined but
    not used [-Wunused-function]
     static int tcpci_suspend(struct device *dev)
                ^~~~~~~~~~~~~
    
    Acked-by: Peter Chen <peter.chen@nxp.com>
    Signed-off-by: Li Jun <jun.li@nxp.com>
    Signed-off-by: Meng Li <Meng.Li@windriver.com>

--- drivers/usb/typec/tcpm/tcpci.c
+++ drivers/usb/typec/tcpm/tcpci.c
@@ -838,7 +838,7 @@ static int tcpci_remove(struct i2c_client *client)
 	return 0;
 }
 
-static int tcpci_suspend(struct device *dev)
+static int __maybe_unused tcpci_suspend(struct device *dev)
 {
 	struct tcpci_chip *chip = dev_get_drvdata(dev);
 
@@ -850,7 +850,7 @@ static int tcpci_suspend(struct device *dev)
 	return 0;
 }
 
-static int tcpci_resume(struct device *dev)
+static int __maybe_unused tcpci_resume(struct device *dev)
 {
 	struct tcpci_chip *chip = dev_get_drvdata(dev);
 
