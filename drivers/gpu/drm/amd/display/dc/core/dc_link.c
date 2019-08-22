a1db71755ec5bcaaee8fe9598fc3a69fd8280384
Martin Tsai <martin.tsai@amd.com>
2019-08-22 10:02:13 +0800

    drm/amd/display: Handle virtual signal type in disable_link()
    commit 616f5b65f1c02d3d6ae370644670d14c57de2fd8 upstream.
    
    [Why]
    The new implementation changed the behavior to allow process setMode
    to DAL when DAL returns empty mode query for unplugged display.
    This will trigger additional disable_link().
    When unplug HDMI from MST dock, driver will update stream->signal to
    "Virtual". disable_link() will call disable_output() if the signal type
    is not DP and induce other displays on MST dock show black screen.
    
    [How]
    Don't need to process disable_output() if the signal type is virtual.
    
    Signed-off-by: Martin Tsai <martin.tsai@amd.com>
    Reviewed-by: Charlene Liu <Charlene.Liu@amd.com>
    Acked-by: Leo Li <sunpeng.li@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/gpu/drm/amd/display/dc/core/dc_link.c
+++ drivers/gpu/drm/amd/display/dc/core/dc_link.c
@@ -2113,8 +2113,10 @@ static void disable_link(struct dc_link *link, enum signal_type signal)
 			dp_disable_link_phy(link, signal);
 		else
 			dp_disable_link_phy_mst(link, signal);
-	} else
-		link->link_enc->funcs->disable_output(link->link_enc, signal);
+	} else {
+		if (signal != SIGNAL_TYPE_VIRTUAL)
+			link->link_enc->funcs->disable_output(link->link_enc, signal);
+	}
 
 	if (signal == SIGNAL_TYPE_DISPLAY_PORT_MST) {
 		/* MST disable link only when no stream use the link */
