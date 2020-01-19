61dec4b9c9b145dd5a7ae4e60607b982b2ac83a5
Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
2020-01-19 10:58:28 -0700

    spi: Add multi-die info in spi_device structure
    commit adf199e79760dcad21a3470385b512509ce28039 from
    https://github.com/Xilinx/linux-xlnx.git
    
    Added multi-die member in spi_device structure.
    Scan for multi-die property in the device tree, if
    present then set the multi_die member of spi_device
    structure variable to true.
    
    If the multi_die member of spi_device structure variable
    is set, then a read transaction that spans across multiple
    banks is split into one read per bank. This resolves the
    die crossover issue.
    
    Signed-off-by: Amit Kumar Mahapatra <amit.kumar-mahapatra@xilinx.com>
    State: pending
    Signed-off-by: Yaliang Wang <Yaliang.Wang@windriver.com>

--- include/linux/spi/spi.h
+++ include/linux/spi/spi.h
@@ -124,6 +124,7 @@ void spi_statistics_add_transfer_stats(struct spi_statistics *stats,
  *	not using a GPIO line)
  * @word_delay_usecs: microsecond delay to be inserted between consecutive
  *	words of a transfer
+ * @multi_die: Flash device with multiple dies.
  *
  * @statistics: statistics for the spi_device
  *
@@ -172,6 +173,7 @@ struct spi_device {
 	int			cs_gpio;	/* LEGACY: chip select gpio */
 	struct gpio_desc	*cs_gpiod;	/* chip select gpio desc */
 	uint8_t			word_delay_usecs; /* inter-word delay */
+	bool			multi_die;	/* flash with multiple dies*/
 
 	/* the statistics */
 	struct spi_statistics	statistics;
