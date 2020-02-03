c3900bd8bc7379e321689a11b772257b1f110ce3
Andy Shevchenko <andriy.shevchenko@linux.intel.com>
2020-02-03 15:31:25 +0200

    console: Drop double check for console_drivers being non-NULL
    commit caa72c3bc584bc28b557bcf1a47532a7a6f37e6f upstream
    
    There is no need to explicitly check for console_drivers to be non-NULL
    since for_each_console() does this.
    
    Link: http://lkml.kernel.org/r/20200203133130.11591-2-andriy.shevchenko@linux.intel.com
    To: linux-kernel@vger.kernel.org
    To: Steven Rostedt <rostedt@goodmis.org>
    Signed-off-by: Andy Shevchenko <andriy.shevchenko@linux.intel.com>
    Reviewed-by: Petr Mladek <pmladek@suse.com>
    Reviewed-by: Sergey Senozhatsky <sergey.senozhatsky@gmail.com>
    Signed-off-by: Petr Mladek <pmladek@suse.com>
    [OP: adjusted context for 5.2-rt]
    Signed-off-by: Ovidiu Panait <ovidiu.panait@windriver.com>

--- kernel/printk/printk.c
+++ kernel/printk/printk.c
@@ -1763,9 +1763,6 @@ static void call_console_drivers(u64 seq, const char *ext_text, size_t ext_len,
 
 	trace_console_rcuidle(text, len);
 
-	if (!console_drivers)
-		return;
-
 	for_each_console(con) {
 		if (!(con->flags & CON_ENABLED))
 			continue;
@@ -2411,18 +2408,17 @@ void register_console(struct console *newcon)
 	struct console_cmdline *c;
 	static bool has_preferred;
 
-	if (console_drivers)
-		for_each_console(bcon)
-			if (WARN(bcon == newcon,
-					"console '%s%d' already registered\n",
-					bcon->name, bcon->index))
-				return;
+	for_each_console(bcon) {
+		if (WARN(bcon == newcon, "console '%s%d' already registered\n",
+					 bcon->name, bcon->index))
+			return;
+	}
 
 	/*
 	 * before we register a new CON_BOOT console, make sure we don't
 	 * already have a valid console
 	 */
-	if (console_drivers && newcon->flags & CON_BOOT) {
+	if (newcon->flags & CON_BOOT) {
 		/* find the last or real console */
 		for_each_console(bcon) {
 			if (!(bcon->flags & CON_BOOT)) {
