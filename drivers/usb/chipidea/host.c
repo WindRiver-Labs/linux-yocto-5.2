boundarydevices/linux

MLK-11269-2 usb: chipidea: host: avoid check PORT_SUSPEND wrongly
At some situations, the ehci_bus_suspend may not set PORT_SUSPEND
due to port is not enabled, so add flag ehci->bus_suspended to
check if ehci_bus_suspend set PORT_SUSPEND or not.

We see "ci_hdrc ci_hdrc.0: timeout waiting for SUSPEND" wrongly when
plug in an unsupported usb device, in that case, the PORT_PE is cleared
and bus_suspend is called.

Signed-off-by: Peter Chen <peter.chen@freescale.com>

drivers/usb/chipidea/host.c
@@ -508,7 +508,8 @@ static int ci_ehci_bus_suspend(struct usb_hcd *hcd)
 			 * If a transaction is in progress, there may be a delay in
 			 * suspending the port. Poll until the port is suspended.
 			 */
-			if (ehci_handshake(ehci, reg, PORT_SUSPEND,
+			if (test_bit(port, &ehci->bus_suspended) &&
+					ehci_handshake(ehci, reg, PORT_SUSPEND,
 							PORT_SUSPEND, 5000))
 				ehci_err(ehci, "timeout waiting for SUSPEND\n");
 

WindRiver-Labs/linux-yocto-5.2

commit f096f0feb48f770c0d454444a4302830e3c3e1a5 from
https://source.codeaurora.org/external/imx/linux-imx.git
(cherry picked from commit 82cfe6c31ee55e5aab1a057a5aaf853b7bb6cd07)
(cherry picked from commit a4fe7e55e84944962d6a7be7bd404865d376811e)
Signed-off-by: Vipul Kumar <vipul_kumar@mentor.com>
Signed-off-by: Srikanth Krishnakar <Srikanth_Krishnakar@mentor.com>
Signed-off-by: Meng Li <Meng.Li@windriver.com>

drivers/usb/chipidea/host.c
@@ -198,7 +198,8 @@ static int ci_imx_ehci_hub_control(
 		 * If a transaction is in progress, there may be a delay in
 		 * suspending the port. Poll until the port is suspended.
 		 */
-		if (ehci_handshake(ehci, status_reg, PORT_SUSPEND,
+		if (test_bit(port, &ehci->bus_suspended) &&
+					ehci_handshake(ehci, status_reg, PORT_SUSPEND,
 						PORT_SUSPEND, 5000))
 			ehci_err(ehci, "timeout waiting for SUSPEND\n");
 