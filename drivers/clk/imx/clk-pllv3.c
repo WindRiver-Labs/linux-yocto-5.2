reMarkable/linux

MLK-12861-1 ARM: imx: support runtime clock management on i.mx7d when M4 is enabled
For i.MX7D, current runtime clock management code will skip all
PLL/PFD/GATE enable/disable when M4 is enabled, this is NOT good
for power number in low power idle and audio playback, as M4 only
uses one high speed PFD which is from system PLL, it is never
disabled runtimely, so we can just enable the hardware operation of
PLL/PFD/GATE for A7.

Signed-off-by: Anson Huang <Anson.Huang@nxp.com>

arch/arm/mach-imx/clk-pllv3.c
@@ -108,7 +108,7 @@ static int clk_pllv3_do_hardware(struct clk_hw *hw, bool enable)
 
 static void clk_pllv3_do_shared_clks(struct clk_hw *hw, bool enable)
 {
-	if (imx_src_is_m4_enabled()) {
+	if (imx_src_is_m4_enabled() && cpu_is_imx6sx()) {
 #ifdef CONFIG_SOC_IMX6SX
 		if (!amp_power_mutex || !shared_mem) {
 			if (enable)

WindRiver-Labs/linux-yocto-5.2

commit 8e9c7a08715d5ad1d323e3bd55e71604d44850f2 from
https://source.codeaurora.org/external/imx/linux-imx.git
(cherry picked from commit 02a2e8d73bcb8d2b8362b4328976dfcdc502a19c)
Signed-off-by: Arulpandiyan Vadivel <arulpandiyan_vadivel@mentor.com>
Signed-off-by: Meng Li <Meng.Li@windriver.com>

drivers/clk/imx/clk-pllv3.c
@@ -104,7 +104,7 @@ static int clk_pllv3_do_hardware(struct clk_hw *hw, bool enable)
 
 static void clk_pllv3_do_shared_clks(struct clk_hw *hw, bool enable)
 {
-	if (imx_src_is_m4_enabled()) {
+	if (imx_src_is_m4_enabled() && clk_on_imx6sx()) {
 #ifdef CONFIG_SOC_IMX6SX
 		if (!amp_power_mutex || !shared_mem) {
 			if (enable)