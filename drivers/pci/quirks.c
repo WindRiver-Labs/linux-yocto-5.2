afb17eff9f1364e2c6a238172ccbc5c7c533ba46
Raymond Pang <RaymondPang-oc@zhaoxin.com>
2020-03-27 17:11:47 +0800

    PCI: Add ACS quirk for Zhaoxin multi-function devices
    commit 0325837c51cb7c9a5bd3e354ac0c0cda0667d50e upstream.
    
    Some Zhaoxin endpoints are implemented as multi-function devices without an
    ACS capability, but they actually don't support peer-to-peer transactions.
    Add ACS quirks to declare DMA isolation.
    
    Link: https://lore.kernel.org/r/20200327091148.5190-3-RaymondPang-oc@zhaoxin.com
    Signed-off-by: Raymond Pang <RaymondPang-oc@zhaoxin.com>
    Signed-off-by: Bjorn Helgaas <bhelgaas@google.com>
    Signed-off-by: Paul Gortmaker <paul.gortmaker@windriver.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -4678,6 +4678,10 @@ static const struct pci_dev_acs_enabled {
 	{ PCI_VENDOR_ID_AMPERE, 0xE00B, pci_quirk_xgene_acs },
 	{ PCI_VENDOR_ID_AMPERE, 0xE00C, pci_quirk_xgene_acs },
 	{ PCI_VENDOR_ID_BROADCOM, 0xD714, pci_quirk_brcm_acs },
+	/* Zhaoxin multi-function devices */
+	{ PCI_VENDOR_ID_ZHAOXIN, 0x3038, pci_quirk_mf_endpoint_acs },
+	{ PCI_VENDOR_ID_ZHAOXIN, 0x3104, pci_quirk_mf_endpoint_acs },
+	{ PCI_VENDOR_ID_ZHAOXIN, 0x9083, pci_quirk_mf_endpoint_acs },
 	{ 0 }
 };
 
