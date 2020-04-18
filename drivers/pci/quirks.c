cb8adc2d53a17b644e4a6896b16e527c750d04be
Kevin Hao <kexin.hao@windriver.com>
2020-04-18 20:18:02 +0800

    PCI: Fix the Cavium ACS quirk
    Both the following two commits touched the Cavium ACS quirk function.
        adff286a3be5 ("PCI: Apply Cavium ACS quirk to ThunderX2 and ThunderX3")
        be120d347fc3 ("PCI: quirks : Apply ACS quirk for all devices")
    
    But when trying to resolve these conflicts in merge commit ed197188f380
    ("Merge branch 'v5.2/standard/base' into v5.2/standard/cn96xx"), the
    wrong codes were picked. This patch restores the correct codes.
    
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- drivers/pci/quirks.c
+++ drivers/pci/quirks.c
@@ -4243,10 +4243,21 @@ static int pci_quirk_amd_sb_acs(struct pci_dev *dev, u16 acs_flags)
 
 static bool pci_quirk_cavium_acs_match(struct pci_dev *dev)
 {
-	if (!pci_is_pcie(dev) || pci_pcie_type(dev) != PCI_EXP_TYPE_ROOT_PORT)
+	if (!pci_is_pcie(dev))
 		return false;
 
-	return (pci_is_pcie(dev) && ((dev->device & 0xf800) == 0xa000));
+	switch (dev->device) {
+	/*
+	 * Effectively selects all downstream ports for whole ThunderX1
+	 * (which represents 8 SoCs).
+	 */
+	case 0xa000 ... 0xa7ff: /* ThunderX1 */
+	case 0xaf84:  /* ThunderX2 */
+	case 0xb884:  /* ThunderX3 */
+		return true;
+	default:
+		return false;
+	}
 }
 
 static int pci_quirk_cavium_acs(struct pci_dev *dev, u16 acs_flags)
