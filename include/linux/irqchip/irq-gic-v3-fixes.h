6944c119897c6bde7c49528c5e5ff16746fb824a
Linu Cherian <lcherian@marvell.com>
2020-07-15 21:12:17 +0530

    irqchip/gic-v3: Add workaround for interrupt loss on IPI
    commit 6d8e7e989063996a13cf8190d4d23bef8ca9a89e from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    RCU stalls has been observed during stress-ng tests on
    OcteonTx2 silicon and the reason being an interrupt loss on an IPI.
    
    As a workaround, added provision to ensure that every IPI has been
    received on the destination core and if an IPI interrupt loss is
    encountered, do resend the IPI.
    
    Identifying a lost interrupt:
    Once an interrupt is raised on an IPI line, it would be either in
    one of the below states.
       A. pending
       B. active
       C. deactivated
    
    Pending/active states can be verified from the corresponding redist
    registers. An interrupt would be in deactivated state either because its
    already processed or its been lost. So to identify the reason for case C,
    per core tx and rx counters has been introduced for each IPI line.
    
    Change-Id: I5ff5886a498d4b4ef6c783f52a9590feecf314b2
    Signed-off-by: Linu Cherian <lcherian@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/32026
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    [Kevin: Change the lock to raw_spinlock_t for rt]
    Signed-off-by: Kevin Hao <kexin.hao@windriver.com>

--- include/linux/irqchip/irq-gic-v3-fixes.h
+++ include/linux/irqchip/irq-gic-v3-fixes.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: GPL-2.0
- * Marvell Silicon gicv3 definitions
+ * Marvell Silicon GICv3/ITS hardware quirks
  *
  * Copyright (C) 2020 Marvell International Ltd.
  *
@@ -10,6 +10,16 @@
 
 #ifndef __LINUX_IRQCHIP_MARVELL_GIC_V3_H
 #define __LINUX_IRQCHIP_MARVELL_GIC_V3_H
+
+#define GICV3_QUIRK_IPI_MISS	(1 << 0)
+
+u32 gic_rdist_pend_reg(int cpu, int offset);
+u32 gic_rdist_active_reg(int cpu, int offset);
+
+void gic_ipi_rxcount_inc(int cpu, int irq);
+void gic_write_sgi1r_retry(int dest_cpu, int irq, u64 val);
+void gic_v3_enable_quirks(void __iomem *base);
+void gic_v3_enable_ipimiss_quirk(void);
 
 struct page *its_prop_alloc_pages(struct rdists *gic_rdists,
 				size_t prop_tbl_sz,  gfp_t gfp_flags);
