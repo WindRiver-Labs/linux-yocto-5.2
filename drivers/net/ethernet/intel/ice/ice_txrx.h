91aa05ce7b739584c05d1209c6cf31fd3ce0b49c
Maciej Fijalkowski <maciej.fijalkowski@intel.com>
2021-01-18 16:13:12 +0100

    ice: move skb pointer from rx_buf to rx_ring
    commit 29b82f2a09d5904420ba7b5fb95a094cf1550bb6 upstream.
    
    Similar thing has been done in i40e, as there is no real need for having
    the sk_buff pointer in each rx_buf. Non-eop frames can be simply handled
    on that pointer moved upwards to rx_ring.
    
    Reviewed-by: Björn Töpel <bjorn.topel@intel.com>
    Signed-off-by: Maciej Fijalkowski <maciej.fijalkowski@intel.com>
    Tested-by: Tony Brelinski <tonyx.brelinski@intel.com>
    Signed-off-by: Tony Nguyen <anthony.l.nguyen@intel.com>
    Signed-off-by: Liwei Song <liwei.song@windriver.com>

--- drivers/net/ethernet/intel/ice/ice_txrx.h
+++ drivers/net/ethernet/intel/ice/ice_txrx.h
@@ -163,7 +163,6 @@ struct ice_tx_offload_params {
 };
 
 struct ice_rx_buf {
-	struct sk_buff *skb;
 	dma_addr_t dma;
 	union {
 		struct {
@@ -299,6 +298,7 @@ struct ice_ring {
 	struct zero_copy_allocator zca;
 	/* CL3 - 3rd cacheline starts here */
 	struct xdp_rxq_info xdp_rxq;
+	struct sk_buff *skb;
 	/* CLX - the below items are only accessed infrequently and should be
 	 * in their own cache line if possible
 	 */
