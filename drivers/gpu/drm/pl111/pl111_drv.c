cbb74d702364187ad4efa6f10e22ddf20de7e2b8
Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
2021-01-26 17:58:34 +0100

    amba: Make the remove callback return void
    All amba drivers return 0 in their remove callback. Together with the
    driver core ignoring the return value anyhow, it doesn't make sense to
    return a value here.
    
    Change the remove prototype to return void, which makes it explicit that
    returning an error value doesn't work as expected. This simplifies changing
    the core remove callback to return void, too.
    
    Reviewed-by: Ulf Hansson <ulf.hansson@linaro.org>
    Reviewed-by: Arnd Bergmann <arnd@arndb.de>
    Acked-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Acked-by: Dmitry Torokhov <dmitry.torokhov@gmail.com>
    Acked-by: Krzysztof Kozlowski <krzk@kernel.org> # for drivers/memory
    Acked-by: Mark Brown <broonie@kernel.org>
    Acked-by: Linus Walleij <linus.walleij@linaro.org>
    Acked-by: Suzuki K Poulose <suzuki.poulose@arm.com> # for hwtracing/coresight
    Acked-By: Vinod Koul <vkoul@kernel.org> # for dmaengine
    Acked-by: Guenter Roeck <linux@roeck-us.net> # for watchdog
    Acked-by: Wolfram Sang <wsa@kernel.org> # for I2C
    Acked-by: Takashi Iwai <tiwai@suse.de> # for sound
    Acked-by: Vladimir Zapolskiy <vz@mleia.com> # for memory/pl172
    Acked-by: Greg Kroah-Hartman <gregkh@linuxfoundation.org>
    Link: https://lore.kernel.org/r/20210126165835.687514-5-u.kleine-koenig@pengutronix.de
    Signed-off-by: Uwe Kleine-König <u.kleine-koenig@pengutronix.de>
    [WK: The original patch got from Marvell sdk11.21.09]
    Signed-off-by: Wenlin Kang <wenlin.kang@windriver.com>

--- drivers/gpu/drm/pl111/pl111_drv.c
+++ drivers/gpu/drm/pl111/pl111_drv.c
@@ -335,7 +335,7 @@ dev_put:
 	return ret;
 }
 
-static int pl111_amba_remove(struct amba_device *amba_dev)
+static void pl111_amba_remove(struct amba_device *amba_dev)
 {
 	struct device *dev = &amba_dev->dev;
 	struct drm_device *drm = amba_get_drvdata(amba_dev);
@@ -347,8 +347,6 @@ static int pl111_amba_remove(struct amba_device *amba_dev)
 	drm_mode_config_cleanup(drm);
 	drm_dev_put(drm);
 	of_reserved_mem_device_release(dev);
-
-	return 0;
 }
 
 /*
