c352f75bc88f000b989504da07602c91ec222c2e
Naveen Mamindlapalli <naveenm@marvell.com>
2021-02-15 13:26:41 +0530

    octeontx2-af: refactor function npc_install_flow for default entry
    commit 12db283ae6ad9acd63e649da6f331081a604edac from
    git@git.assembla.com:cavium/WindRiver.linux.git
    
    This patch refactors npc_install_flow to install default entry
    similar to other MCAM entries. That way the code is more readable.
    Modified npc_verify_entry and npc_verify_channel to correctly check
    MCAM rules installed by AF.
    
    Change-Id: I7d4ca5585ced181e75e3482fd409a89d6e4978b3
    Signed-off-by: Naveen Mamindlapalli <naveenm@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/kernel/linux/+/46016
    Tested-by: sa_ip-sw-jenkins <sa_ip-sw-jenkins@marvell.com>
    Reviewed-by: Sunil Kovvuri Goutham <Sunil.Goutham@cavium.com>
    Integrated-by: Abhishek Paliwal <paliwal.abhishek@windriver.com>

--- drivers/net/ethernet/marvell/octeontx2/af/rvu_npc_fs.c
+++ drivers/net/ethernet/marvell/octeontx2/af/rvu_npc_fs.c
@@ -1008,31 +1008,21 @@ static int npc_install_flow(struct rvu *rvu, int blkaddr, u16 target,
 	if (is_npc_intf_tx(req->intf))
 		goto find_rule;
 
-	if (def_ucast_rule)
+	if (req->default_rule) {
+		entry_index = npc_get_nixlf_mcam_index(mcam, target, nixlf,
+						       NIXLF_UCAST_ENTRY);
+		enable = is_mcam_entry_enabled(rvu, mcam, blkaddr, entry_index);
+	}
+
+	/* update mcam entry with default unicast rule attributes */
+	if (def_ucast_rule &&
+	    (msg_from_vf || (req->default_rule && req->append))) {
 		missing_features = (def_ucast_rule->features ^ features) &
 					def_ucast_rule->features;
-
-	if (req->default_rule && req->append) {
-		/* add to default rule */
 		if (missing_features)
 			npc_update_flow(rvu, entry, missing_features,
 					&def_ucast_rule->packet, &def_ucast_rule->mask,
 					&dummy, req->intf);
-		enable = rvu_npc_write_default_rule(rvu, blkaddr,
-						    nixlf, target,
-						    pfvf->nix_rx_intf, entry,
-						    &entry_index);
-		installed_features = req->features | missing_features;
-	} else if (req->default_rule && !req->append) {
-		/* overwrite default rule */
-		enable = rvu_npc_write_default_rule(rvu, blkaddr,
-						    nixlf, target,
-						    pfvf->nix_rx_intf, entry,
-						    &entry_index);
-	} else if (msg_from_vf) {
-		/* normal rule - include default rule also to it for VF */
-		npc_update_flow(rvu, entry, missing_features, &def_ucast_rule->packet,
-				&def_ucast_rule->mask, &dummy, req->intf);
 		installed_features = req->features | missing_features;
 	}
 find_rule:
@@ -1043,12 +1033,9 @@ find_rule:
 			return -ENOMEM;
 		new = true;
 	}
-	/* no counter for default rule */
-	if (req->default_rule)
-		goto update_rule;
 
 	/* allocate new counter if rule has no counter */
-	if (req->set_cntr && !rule->has_cntr)
+	if (!req->default_rule && req->set_cntr && !rule->has_cntr)
 		rvu_mcam_add_counter_to_rule(rvu, owner, rule, rsp);
 
 	/* if user wants to delete an existing counter for a rule then
@@ -1058,7 +1045,7 @@ find_rule:
 		rvu_mcam_remove_counter_from_rule(rvu, owner, rule);
 
 	write_req.hdr.pcifunc = owner;
-	write_req.entry = req->entry;
+	write_req.entry = entry_index;
 	write_req.intf = req->intf;
 	write_req.enable_entry = (u8)enable;
 	/* if counter is available then clear and use it */
@@ -1076,7 +1063,7 @@ find_rule:
 			kfree(rule);
 		return err;
 	}
-update_rule:
+	/* update rule */
 	memcpy(&rule->packet, &dummy.packet, sizeof(rule->packet));
 	memcpy(&rule->mask, &dummy.mask, sizeof(rule->mask));
 	rule->entry = entry_index;
